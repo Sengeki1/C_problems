dd73e807f6aa5b076316240737f7081e948f866a
Sengeki1 <gamersengeki@gmail.com>
2023-06-23 16:31:16 -0100

    redefined

--- Threads/1.c
+++ Threads/1.c
@@ -2,12 +2,17 @@
 #include <stdio.h>
 #include <pthread.h> // posix thread library
 
-void *computation();
+void *computation(void*);
 int main() {
     pthread_t thread1; // create thread
+    pthread_t thread2;
+
+    long value1 = 1;
+    long value2 = 2;
 
     // Run computation function in a separated thread
-    pthread_create(&thread1, NULL, computation, NULL);
+    pthread_create(&thread1, NULL, computation, (void*) &value1);
+    pthread_create(&thread2, NULL, computation, (void*) &value2);
     /*
     
      We are passing in a pointer to our thread variable, the second argument is where we set
@@ -15,7 +20,8 @@ int main() {
     fourth argument could be used to pass arguments to the computation function 
     
     */
-   pthread_join(thread1, NULL); // Join the thread execution back with our main thread when its done
+    pthread_join(thread1, NULL); // Join the thread execution back with our main thread when its done
+    pthread_join(thread2, NULL);
    /*
 
     The first argument is the thread we are joining with the main thread of execution and the
@@ -23,7 +29,11 @@ int main() {
 
    */
 }
-void *computation() {
-    printf("Computation\n");
+void *computation(void* add) {
+    long sum = 0;
+    long *add_num = (long*) (add);
+    for(long i = 0; i <10000000; i++)
+        sum += *add_num;
+
     return NULL;
 }
